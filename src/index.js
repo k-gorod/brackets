1ec57a39c81c8f910e9e28fef1bf7c562c93a31b
Kostya Gorodnichuk <coolkostya@gmail.com>
2020-03-22 18:46:15 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var ret = false;
+  let arr = bracketsConfig.map( (e) => e.join("") );
+  for (let i=0;i<arr.length;i++){
+    if(str.includes(arr[i])){
+      str=str.replace(arr[i],"");
+      i =-1;
+    }
+  }
+  if(str.length==0){ret=true}
+  return ret;
 }
